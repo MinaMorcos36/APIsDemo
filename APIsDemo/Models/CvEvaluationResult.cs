8181e8acb5f582981ea82731fc4c4e92727e4489
Mina Morcos <126086630+MinaMorcos36@users.noreply.github.com>
2026-04-22 21:41:57 +0200

    Revert "Updates"

--- APIsDemo/Models/CvEvaluationResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIsDemo.Models;
-
-public class CvEvaluationResult
-{
-    public int Score { get; set; }
-    public string Reason { get; set; } = string.Empty;
-
-    public bool Shortlisted => Score >= 70;
-}
--- APIsDemo/Program.cs
+++ APIsDemo/Program.cs
@@ -1,7 +1,4 @@
 using APIsDemo.Models;
-using APIsDemo.Services;
-using APIsDemo.Services.Implementations;
-using APIsDemo.Services.Interfaces;
 using APIsDemo.Services.Implementations.Authentication;
 using APIsDemo.Services.Implementations.Community;
 using APIsDemo.Services.Interfaces.Authentication;
@@ -11,7 +8,6 @@ using APIsDemo.Services.Implementations.Admin;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.SemanticKernel;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,19 +19,6 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddSingleton<FileParsingService>();
-builder.Services.AddSingleton<LanguageDetectionService>();
-builder.Services.AddSingleton<CvProcessingService>();
-builder.Services.AddSingleton<GeminiCvEvaluationService>();
-builder.Services.AddScoped<CareerChatService>();
-
-
-builder.Services.AddKernel();
-builder.Services.AddGoogleAIGeminiChatCompletion(builder.Configuration["AI:Gemini:Model"],
-builder.Configuration["AI:Gemini:Apikey"]);
-
-
-
 
 var jwtSettings = builder.Configuration.GetSection("JWT");
 builder.Services.AddAuthentication(options =>
